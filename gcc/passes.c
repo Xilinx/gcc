563c74bbed7d1c73a19cacd5f89878a860578a51
bje <bje@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-05-05 06:07:31 +0000

    Merge with mainline.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/named-addr-spaces-branch@134943 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/passes.c
+++ gcc/passes.c
@@ -553,6 +553,7 @@ init_optimization_passes (void)
   /* These passes are run after IPA passes on every function that is being
      output to the assembler file.  */
   p = &all_passes;
+  NEXT_PASS (pass_O0_always_inline);
   NEXT_PASS (pass_all_optimizations);
     {
       struct opt_pass **p = &pass_all_optimizations.pass.sub;
@@ -999,11 +1000,15 @@ execute_todo (unsigned int flags)
      to analyze side effects.  The full removal is done just at the end
      of IPA pass queue.  */
   if (flags & TODO_remove_functions)
-    cgraph_remove_unreachable_nodes (true, dump_file);
+    {
+      gcc_assert (!cfun);
+      cgraph_remove_unreachable_nodes (true, dump_file);
+    }
 
   if ((flags & TODO_dump_cgraph)
       && dump_file && !current_function_decl)
     {
+      gcc_assert (!cfun);
       dump_cgraph (dump_file);
       /* Flush the file.  If verification fails, we won't be able to
 	 close the file before aborting.  */
@@ -1162,7 +1167,7 @@ execute_one_ipa_transform_pass (struct cgraph_node *node,
   pass_init_dump_file (pass);
 
   /* Run pre-pass verification.  */
-  execute_todo (pass->todo_flags_start);
+  execute_todo (ipa_pass->function_transform_todo_flags_start);
 
   /* If a timevar is present, start it.  */
   if (pass->tv_id)
