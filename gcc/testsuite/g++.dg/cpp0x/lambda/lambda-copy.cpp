36f1a80fe47b5bbcf8532dc5011b609ba96d4d0d
jfreeman <jfreeman@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-07-16 15:34:58 +0000

    better tests
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/cxx0x-lambdas-branch@137885 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp0x/lambda/lambda-copy.cpp
+++ gcc/testsuite/g++.dg/cpp0x/lambda/lambda-copy.cpp
@@ -1,12 +1,13 @@
 // { dg-do "compile" }
 // { dg-options "-std=c++0x"}
+#include <cassert>
 
 template<typename F>
 void call(F f) { f(); }
 
 int main() {
   int i = 1, j = 2;
-  call([i, j] () -> void { i = 0; j = 0; });
+  call([i, j] () -> void { i = 3; j = 4; });
   assert(i == 1);
   assert(j == 2);
 
--- gcc/testsuite/g++.dg/cpp0x/lambda/lambda-ref.cpp
+++ gcc/testsuite/g++.dg/cpp0x/lambda/lambda-ref.cpp
@@ -1,14 +1,15 @@
 // { dg-do "compile" }
 // { dg-options "-std=c++0x"}
+#include <cassert>
 
 template<typename F>
 void call(F f) { f(); }
 
 int main() {
   int i = 1, j = 2;
-  call([&i, &j] () -> void { i = 0; j = 0; });
-  assert(i == 0);
-  assert(j == 0);
+  call([&i, &j] () -> void { i = 3; j = 4; });
+  assert(i == 3);
+  assert(j == 4);
 
   return 0;
 }
