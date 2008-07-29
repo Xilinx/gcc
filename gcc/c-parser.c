cc866fe95ba9b9a4f942ae8552b4b66c8b9d3a55
bje <bje@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-07-29 06:56:52 +0000

    * c-parser.c (c_parser_postfix_expression_after_paren_type): Guard 	against the error_mark_node.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/named-addr-spaces-branch@138231 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-parser.c
+++ gcc/c-parser.c
@@ -5513,7 +5513,7 @@ c_parser_postfix_expression_after_paren_type (c_parser *parser,
   finish_init ();
   maybe_warn_string_init (type, init);
 
-  if (TYPE_ADDR_SPACE (type) && current_function_decl)
+  if (type != error_mark_node && TYPE_ADDR_SPACE (type) && current_function_decl)
     {
       error ("compound literal qualified by address-space qualifier");
       type = error_mark_node;
