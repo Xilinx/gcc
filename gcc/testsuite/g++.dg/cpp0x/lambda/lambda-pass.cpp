8417c949cf046cf1ecf959a383be6ef908197ec6
jfreeman <jfreeman@138bc75d-0d04-0410-961f-82ee72b054a4>
2008-09-04 19:24:19 +0000

    commented out portion
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/cxx0x-lambdas-branch@140000 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/cpp0x/lambda/lambda-pass.cpp
+++ gcc/testsuite/g++.dg/cpp0x/lambda/lambda-pass.cpp
@@ -1,20 +1,25 @@
 // { dg-do "compile" }
 // { dg-options "-std=c++0x"}
+
 #include <cassert>
 #include <algorithm>
 
+/*
 template <typename F, typename A1>
 void call(F f, const A1& arg1) {
   f(arg1);
 }
+*/
 
 int main() {
+  /*
   int i = 1;
   call(
       [&i] (int j) -> void { i = j; },
       2
   );
   assert(i == 2);
+  */
 
   int A[] = {1, 2, 3, 4};
   int sum = 0;
