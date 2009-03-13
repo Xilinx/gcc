1b1e519fd5a080e45dcd42cbec18b5b2180c64cf
bstarynk <bstarynk@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-03-13 14:54:42 +0000

    2009-03-13  Basile Starynkevitch  <basile@starynkevitch.net> 	[tmallbuf.c testsuite is parsed by ana-base.bysl] 	* melt/ana-base.bysl: added several gimple patterns. 	* testsuite/gcc/tmallbuf.c: indented & simplified.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/melt-branch@144838 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/melt/tmallbuf.c
+++ gcc/testsuite/melt/tmallbuf.c
@@ -18,7 +18,7 @@ alloctab (int n, int y)
   int i = 0;
   int *p = NULL;
   if (n <= 0)
-    return NULL;
+    n = 1;
   p = calloc ((size_t) n, sizeof (int));
   for (i = 0; i < n; i++)
     p[i] = i * 2 + y;
@@ -32,7 +32,7 @@ main (void)
   int k = 10;
   int *t = NULL;
   t = alloctab (k, 17);
-  for (j=0; j<k; j+=2) 
+  for (j = 0; j < k; j += 2)
     t[j] *= 2;
   /* following instruction access t out of bounds */
   t[k + 1] = 2 * k;
