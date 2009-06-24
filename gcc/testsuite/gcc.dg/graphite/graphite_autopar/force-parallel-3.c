2a2351e4f57e74782f3fd4ffecb6256acd782c5d
lifeng <lifeng@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-06-24 05:38:40 +0000

    Update -fdump-tree-final_cleanup to -fdump-tree-optimized.
    2009-06-24  Li Feng  <nemokingdom@gmail.com>
    
    	* testsuite/gcc.dg/graphite/graphite_autopar/force-parallel-1.c: Update
    	-fdump-tree-final_cleanup to -fdump-tree-optimized.
    	* testsuite/gcc.dg/graphite/graphite_autopar/force-parallel-2.c: Ditto.
    	* testsuite/gcc.dg/graphite/graphite_autopar/force-parallel-3.c: Ditto.
    	* testsuite/gcc.dg/graphite/graphite_autopar/graphite_autopar.exp: Ditto.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/graphite@148891 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/graphite/graphite_autopar/force-parallel-3.c
+++ gcc/testsuite/gcc.dg/graphite/graphite_autopar/force-parallel-3.c
@@ -23,6 +23,6 @@ int main(void)
 }
 
 /* Check that parallel code generation part make the right answer.  */
-/* { dg-final { scan-tree-dump-times "loopfn" 5 "final_cleanup" } } */
+/* { dg-final { scan-tree-dump-times "loopfn" 5 "optimized" } } */
 /* { dg-final { cleanup-tree-dump "parloops" } } */
-/* { dg-final { cleanup-tree-dump "final_cleanup" } } */
+/* { dg-final { cleanup-tree-dump "optimized" } } */
