9f6f65ac601b8f3e9ed09c7ba344380eeac5db9e
bstarynk <bstarynk@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-07-22 14:28:44 +0000

    2009-07-22  Basile Starynkevitch  <basile@starynkevitch.net> 	* testsuite/melt/tgreenprint.c: indented and all functions 	prefixed for clarity with tGreenPrint. 	* melt/ana-base.melt: added cfun_decl cfun_gimple_body 	cfun_has_cfg cfun_local_decls cfun_nonlocal_goto_save_area 	cfun_static_chain_decl has_cfun. each_bb_cfun made safer. 	* melt/ana-simple.melt: greenpass is now after phiopt.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/melt-branch@149934 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/melt/tgreenprint.c
+++ gcc/testsuite/melt/tgreenprint.c
@@ -15,33 +15,33 @@
 
 #include <stdlib.h>
 
-void simple_fprint_stdout(int p)
+extern FILE *tGreenPrint_otherfile;
+
+void
+tGreenPrint_simple_fprint_stdout (int p)
 {
   fprintf (stdout, "simple fprint stdout p=%d\n", p);
 }
 
 static inline void
-say_str_int (FILE* f, const char* str, int n)
+tGreenPrint_say_str_int (FILE * f, const char *str, int n)
 {
   if (f)
     {
-      fprintf(f, "say str=%s n=%d\n", str, n);
+      fprintf (f, "say str=%s n=%d\n", str, n);
       fflush (f);
     }
 }
 
-extern FILE* otherfile;
 
-void green_test(void)
+void
+tGreenPrint_green_test (void)
 {
-  if (!otherfile)
-    otherfile = tmpfile ();
-  say_str_int (stdout, "green_test stdout", __LINE__);
-  say_str_int (stderr, "green_test stderr", __LINE__);
-  say_str_int ((FILE*)0, "green_test nullfile", __LINE__);
-  say_str_int (otherfile, "green_test otherfile", __LINE__);
+  if (!tGreenPrint_otherfile)
+    tGreenPrint_otherfile = tmpfile ();
+  tGreenPrint_say_str_int (stdout, "green_test stdout", __LINE__);
+  tGreenPrint_say_str_int (stderr, "green_test stderr", __LINE__);
+  tGreenPrint_say_str_int ((FILE *) 0, "green_test nullfile", __LINE__);
+  tGreenPrint_say_str_int (tGreenPrint_otherfile, "green_test otherfile",
+			   __LINE__);
 }
-
-
-  
-  
