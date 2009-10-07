954a1850b02ae819321c9e89f1f28c0393b3fbd4
spop <spop@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-10-07 00:59:03 +0000

    Handle interchange in distributed swim.
    2009-10-06  Sebastian Pop  <sebastian.pop@amd.com>
    
    	* graphite-dependences.c (reduction_dr_1): New.
    	(reduction_dr_p): New.
    	(graphite_legal_transform_dr): Call reduction_dr_p.
    	(reduction_ddr): Renamed reduction_ddr_p.
    	* graphite-poly.h (same_pdr_p): New.
    	(number_of_write_pdrs): New.
    	* graphite-sese-to-poly.c (nb_data_writes_in_bb): New.
    	(split_reduction_stmt): Do not split reduction statements
    	when there are no writes to memory.
    	(translate_scalar_reduction_to_array_for_stmt): Insert the
    	memory reduction statement just after the scalar reduction statement.
    
    	* gcc.dg/graphite/interchange-10.c: Updated to differ from interchange-4.c.
    	Un-XFAIL-ed.
    	* gcc.dg/graphite/interchange-3.c: Un-XFAIL-ed.
    	* gcc.dg/graphite/interchange-4.c: Un-XFAIL-ed.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/graphite@152509 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/graphite/interchange-10.c
+++ gcc/testsuite/gcc.dg/graphite/interchange-10.c
@@ -1,5 +1,3 @@
-/* Formerly known as ltrans-1.c */
-
 double u[1782225];
 int foo(int N, int *res)
 {
@@ -11,10 +9,11 @@ int foo(int N, int *res)
       sum = sum + u[i + 1335 * j];
 
   for (i = 0; i < N; i++)
-    u[1336 * i] *= 2;
+    for (j = 0; j < N; j++)
+      sum = sum + u[i + 1335 * j];
 
   *res = sum + N;
 }
 
-/* { dg-final { scan-tree-dump-times "will be interchanged" 1 "graphite" { xfail *-*-* } } } */
+/* { dg-final { scan-tree-dump-times "will be interchanged" 2 "graphite" } } */
 /* { dg-final { cleanup-tree-dump "graphite" } } */
