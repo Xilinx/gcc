7d4c979443182238c7e15472a9b251bac7e9d3fc
janus <janus@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-11-07 13:32:33 +0000

    2009-11-07  Janus Weil  <janus@gcc.gnu.org>
    	* intrinsic.h (gfc_resolve_extends_type_of): Add prototype.
    	* intrinsic.c (add_functions): Use 'gfc_resolve_extends_type_of'.
    	* iresolve.c (gfc_resolve_extends_type_of): New function, which
    	replaces the call to EXTENDS_TYPE_OF by the library function
    	'is_extension_of' and modifies the arguments.
    	* trans-intrinsic.c (gfc_conv_extends_type_of): Removed.
    	(gfc_conv_intrinsic_function): FOR EXTENDS_TYPE_OF, don't call
    	gfc_conv_extends_type_of but gfc_conv_intrinsic_funcall.
    
    
    2009-11-07  Janus Weil  <janusw@gcc.gnu.org>
    
    	* gfortran.map: Add _gfortran_is_extension_of.
    	* Makefile.am: Add intrinsics/extends_type_of.c.
    	* Makefile.in: Regenerated.
    	* intrinsics/extends_type_of.c: New file.
    
    
    2009-11-07  Janus Weil  <janusw@gcc.gnu.org>
    
    	* gfortran.dg/extends_type_of_1.f03: New test.
    	* gfortran.dg/same_type_as_1.f03: Extended.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/fortran-dev@153994 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/fortran/trans-intrinsic.c
+++ gcc/fortran/trans-intrinsic.c
@@ -4745,21 +4745,6 @@ gfc_conv_same_type_as (gfc_se *se, gfc_expr *expr)
 }
 
 
-/* Generate code for the EXTENDS_TYPE_OF intrinsic.  */
-
-static void
-gfc_conv_extends_type_of (gfc_se *se, gfc_expr *expr)
-{
-  gfc_expr *e;
-  /* TODO: Implement EXTENDS_TYPE_OF.  */
-  gfc_error ("Intrinsic EXTENDS_TYPE_OF at %L not yet implemented",
-	     &expr->where);
-  /* Just return 'false' for now.  */
-  e = gfc_logical_expr (false, &expr->where);
-  gfc_conv_expr (se, e);
-}
-
-
 /* Generate code for SELECTED_CHAR_KIND (NAME) intrinsic function.  */
 
 static void
@@ -5172,10 +5157,6 @@ gfc_conv_intrinsic_function (gfc_se * se, gfc_expr * expr)
       gfc_conv_same_type_as (se, expr);
       break;
 
-    case GFC_ISYM_EXTENDS_TYPE_OF:
-      gfc_conv_extends_type_of (se, expr);
-      break;
-
     case GFC_ISYM_ABS:
       gfc_conv_intrinsic_abs (se, expr);
       break;
@@ -5553,6 +5534,7 @@ gfc_conv_intrinsic_function (gfc_se * se, gfc_expr * expr)
     case GFC_ISYM_CHMOD:
     case GFC_ISYM_DTIME:
     case GFC_ISYM_ETIME:
+    case GFC_ISYM_EXTENDS_TYPE_OF:
     case GFC_ISYM_FGET:
     case GFC_ISYM_FGETC:
     case GFC_ISYM_FNUM:
