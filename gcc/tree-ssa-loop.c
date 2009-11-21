4d1b007e3ab61b4362a2f23032466e067838f8b6
spop <spop@138bc75d-0d04-0410-961f-82ee72b054a4>
2009-11-21 01:45:19 +0000

    Re-enable Graphite passes at -O1 and below.
    2009-11-20  Sebastian Pop  <sebastian.pop@amd.com>
    
    	Revert patch from 2009-09-14.
    	* tree-ssa-loop.c (gate_graphite_transforms): Re-enable Graphite
    	passes at -O1 and below.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/graphite@154393 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa-loop.c
+++ gcc/tree-ssa-loop.c
@@ -311,10 +311,6 @@ gate_graphite_transforms (void)
       || flag_graphite_identity || flag_loop_parallelize_all)
     flag_graphite = 1;
 
-  /* Disable Graphite passes at -O1 and below.  */
-  if (optimize < 2)
-    flag_graphite = 0;
-
   return flag_graphite != 0;
 }
 
