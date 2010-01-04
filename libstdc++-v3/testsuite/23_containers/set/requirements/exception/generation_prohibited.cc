14f02f29c87a1dad04bce2f4e73f81c13322e114
bstarynk <bstarynk@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-01-04 07:17:41 +0000

    2010-01-04  Basile Starynkevitch  <basile@starynkevitch.net>
            MELT branch merged with trunk rev 155603
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/melt-branch@155605 138bc75d-0d04-0410-961f-82ee72b054a4

--- libstdc++-v3/testsuite/23_containers/set/requirements/exception/generation_prohibited.cc
+++ libstdc++-v3/testsuite/23_containers/set/requirements/exception/generation_prohibited.cc
@@ -1,41 +1,35 @@
-// 2006-06-04  Stephen M. Webb <stephen.webb@bregmasoft.com>
-//
-// Copyright (C) 2006, 2009 Free Software Foundation, Inc.
+// { dg-options "-std=gnu++0x" }
+// { dg-require-cstdint "" }
+
+// 2009-09-09  Benjamin Kosnik  <benjamin@redhat.com>
+
+// Copyright (C) 2009 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
 // terms of the GNU General Public License as published by the
 // Free Software Foundation; either version 3, or (at your option)
 // any later version.
-//
+
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-//
+
 // You should have received a copy of the GNU General Public License along
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// { dg-do run { xfail *-*-* } }
-// { dg-options "-D_GLIBCXX_DEBUG" }
+#include <set>
+#include <exception/safety.h>
 
-// 5.1.7.1 class template uniform_int
-// 5.1.7.1 [2] constructors and member functions
-
-#include <tr1/random>
-#include <testsuite_hooks.h>
-
-void
-test01() 
-{ 
-  using namespace std::tr1;
-
-  uniform_int<int> u(20, 1);
-}
-
+// Container requirement testing, exceptional behavior
 int main()
 {
-  test01();
+  typedef __gnu_cxx::throw_value_random value_type;
+  typedef __gnu_cxx::throw_allocator_random<value_type> allocator_type;
+  typedef std::less<value_type> compare_type;
+  typedef std::set<value_type, compare_type, allocator_type> test_type;
+  __gnu_test::generation_prohibited<test_type> test;
   return 0;
 }
--- libstdc++-v3/testsuite/23_containers/unordered_set/requirements/exception/generation_prohibited.cc
+++ libstdc++-v3/testsuite/23_containers/unordered_set/requirements/exception/generation_prohibited.cc
@@ -1,41 +1,36 @@
-//
-// 2008-11-24  Edward M. Smith-Rowland <3dw4rd@verizon.net>
-//
-// Copyright (C) 2008, 2009 Free Software Foundation, Inc.
+// { dg-options "-std=gnu++0x" }
+// { dg-require-cstdint "" }
+
+// 2009-09-09  Benjamin Kosnik  <benjamin@redhat.com>
+
+// Copyright (C) 2009 Free Software Foundation, Inc.
 //
 // This file is part of the GNU ISO C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
 // terms of the GNU General Public License as published by the
 // Free Software Foundation; either version 3, or (at your option)
 // any later version.
-//
+
 // This library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-//
+
 // You should have received a copy of the GNU General Public License along
 // with this library; see the file COPYING3.  If not see
 // <http://www.gnu.org/licenses/>.
 
-// { dg-do run { xfail *-*-* } }
-// { dg-options "-std=c++0x -D_GLIBCXX_DEBUG" }
-// { dg-require-cstdint "" }
+#include <unordered_set>
+#include <exception/safety.h>
 
-// 26.4.8.1.1 Class template uniform_real_distribution [rand.dist_uni]
-// 26.4.2.4 Concept RandomNumberDistribution [rand.concept.dist]
-
-#include <random>
-#include <testsuite_hooks.h>
-
-void
-test01()
-{
-  std::uniform_real_distribution<double> u(5.0, 0.0);
-}
-
+// Container requirement testing, exceptional behavior
 int main()
 {
-  test01();
+  typedef __gnu_cxx::throw_value_random value_type;
+  typedef __gnu_cxx::throw_allocator_random<value_type> allocator_type;
+  typedef std::hash<value_type> hash_type;
+  typedef std::equal_to<value_type> pred_type;
+  typedef std::unordered_set<value_type, hash_type, pred_type, allocator_type> test_type;
+  __gnu_test::generation_prohibited<test_type> test;
   return 0;
 }
