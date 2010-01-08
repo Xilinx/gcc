cedb8a67cb63896529c2f8a575b1fa7dce56cd23
spop <spop@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-01-08 06:04:41 +0000

    Fix PR42521.
    2010-01-07  Sebastian Pop  <sebastian.pop@amd.com>
    
    	PR middle-end/42521
    	* graphite.c (graphite_finalize): Call scev_reset.
    	(graphite_transform_loops): Do not call scev_reset between the code
    	generation of scops.
    
    	* testsuite/gcc.dg/graphite/pr42521.c: New.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/graphite@155727 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/graphite.c
+++ gcc/graphite.c
@@ -229,6 +229,7 @@ graphite_finalize (bool need_cfg_cleanup_p)
 {
   if (need_cfg_cleanup_p)
     {
+      scev_reset ();
       cleanup_tree_cfg ();
       profile_status = PROFILE_ABSENT;
       release_recorded_exits ();
@@ -280,10 +281,7 @@ graphite_transform_loops (void)
 	check_poly_representation (scop);
 
       if (transform_done)
-	{
-	  scev_reset ();
-	  need_cfg_cleanup_p = true;
-	}
+	need_cfg_cleanup_p = true;
     }
 
   htab_delete (bb_pbb_mapping);
