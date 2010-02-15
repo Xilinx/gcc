ced7497a9b2ebb88aa20213e4e9482842eb461c7
bstarynk <bstarynk@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-02-15 17:13:38 +0000

    2010-02-15  Basile Starynkevitch  <basile@starynkevitch.net>
    	MELT branch merged with trunk rev 156774
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/melt-branch@156777 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cp/tree.c
+++ gcc/cp/tree.c
@@ -2060,7 +2060,9 @@ cp_tree_equal (tree t1, tree t2)
 	       arg2 = next_call_expr_arg (&iter2))
 	  if (!cp_tree_equal (arg1, arg2))
 	    return false;
-	return (arg1 || arg2);
+	if (arg1 || arg2)
+	  return false;
+	return true;
       }
 
     case TARGET_EXPR:
