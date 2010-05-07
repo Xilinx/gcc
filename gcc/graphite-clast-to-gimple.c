cfbd78f13edc0fa270b18db631ff1ca4451c2950
spop <spop@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-05-07 19:09:14 +0000

    Fix type of integer_one_node for fold_build of POINTER_PLUS_EXPR.
    2010-05-07  Sebastian Pop  <sebastian.pop@amd.com>
    
    	* graphite-clast-to-gimple.c (graphite_create_new_loop_guard): Fix
    	type of integer_one_node for fold_build of POINTER_PLUS_EXPR.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/graphite@159166 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/graphite-clast-to-gimple.c
+++ gcc/graphite-clast-to-gimple.c
@@ -1034,16 +1034,16 @@ graphite_create_new_loop_guard (sese region, edge entry_edge,
      loop iteration count of "PARAMETER - 1".  For PARAMETER == 0 this becomes
      2^{32|64}, and the condition lb <= ub is true, even if we do not want this.
      However lb < ub + 1 is false, as expected.  */
-  tree one;
-  mpz_t gmp_one;
-  
-  mpz_init (gmp_one);
-  mpz_set_si (gmp_one, 1);
-  one = gmp_cst_to_tree (type, gmp_one);
-  mpz_clear (gmp_one);
-
-  ub_one = fold_build2 (POINTER_TYPE_P (type) ? POINTER_PLUS_EXPR : PLUS_EXPR,
-			type, ub, one);
+  if (POINTER_TYPE_P (type))
+    {
+      tree one = fold_convert (sizetype, integer_one_node);
+      ub_one = fold_build2 (POINTER_PLUS_EXPR, type, ub, one);
+    }
+  else
+    {
+      tree one = fold_convert (type, integer_one_node);
+      ub_one = fold_build2 (PLUS_EXPR, type, ub, one);
+    }
 
   /* When ub + 1 wraps around, use lb <= ub.  */
   if (integer_zerop (ub_one))
