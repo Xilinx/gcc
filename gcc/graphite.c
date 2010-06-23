87b26d0c95d2c10c1a54376611a4ccece86f183b
spop <spop@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-06-23 20:43:29 +0000

    Add more debug counter guards.
    2010-06-23  Sebastian Pop  <sebastian.pop@amd.com>
    
    	* graphite.c (graphite_transform_loops): Add two more dbg_cnt calls.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/graphite@161290 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/graphite.c
+++ gcc/graphite.c
@@ -272,13 +272,16 @@ graphite_transform_loops (void)
   sbitmap_zero (reductions);
 
   for (i = 0; VEC_iterate (scop_p, scops, i, scop); i++)
-    rewrite_commutative_reductions_out_of_ssa (SCOP_REGION (scop), reductions);
+    if (dbg_cnt (graphite_scop))
+      rewrite_commutative_reductions_out_of_ssa (SCOP_REGION (scop),
+						 reductions);
 
   for (i = 0; VEC_iterate (scop_p, scops, i, scop); i++)
-    {
-      rewrite_reductions_out_of_ssa (scop);
-      build_scop_bbs (scop, reductions);
-    }
+    if (dbg_cnt (graphite_scop))
+      {
+	rewrite_reductions_out_of_ssa (scop);
+	build_scop_bbs (scop, reductions);
+      }
 
   sbitmap_free (reductions);
 
