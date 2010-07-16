162626078023abf9477a7e32c3a79d4cc80e24c6
spop <spop@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-07-16 07:02:55 +0000

    Remove uses of loop->single_iv.
    2010-07-15  Sebastian Pop  <sebastian.pop@amd.com>
    
    	* cfgloop.c (alloc_loop): Remove initialization of loop->single_iv.
    	* cfgloop.h (struct loop): Remove single_iv field.
    	* graphite-sese-to-poly.c (graphite_loop_normal_form): Removed.
    	(scop_canonicalize_loops): Removed.
    	(scop_ivs_can_be_represented): Do not use loop->single_iv.  Iterate
    	over all the loop phi nodes in loop->header.
    	(build_poly_scop): Remove use of scop_canonicalize_loops.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/graphite@162246 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cfgloop.c
+++ gcc/cfgloop.c
@@ -339,7 +339,6 @@ alloc_loop (void)
   loop->exits = GGC_CNEW (struct loop_exit);
   loop->exits->next = loop->exits->prev = loop->exits;
   loop->can_be_parallel = false;
-  loop->single_iv = NULL_TREE;
 
   return loop;
 }
