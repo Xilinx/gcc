039ef0a83214407aff36b99d3916701a5b622858
redbrain <redbrain@crules.org>
2010-08-13 17:22:54 +0100

    some tests

---
+++ libgpython/t_exception.py
@@ -0,0 +1,16 @@
+class B:
+    pass
+class C(B):
+    pass
+class D(C):
+    pass
+
+for c in [B, C, D]:
+    try:
+        raise c()
+    except D:
+        print "D"
+    except C:
+        print "C"
+    except B:
+        print "B"
