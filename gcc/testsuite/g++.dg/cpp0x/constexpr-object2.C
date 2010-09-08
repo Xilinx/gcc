7c10cca2afe6037dad62e0a88d963a2c10b18a0f
Jason Merrill <jason@redhat.com>
2010-09-08 15:48:04 -0400

    adjust expected testresults

--- gcc/testsuite/g++.dg/cpp0x/constexpr-object2.C
+++ gcc/testsuite/g++.dg/cpp0x/constexpr-object2.C
@@ -4,9 +4,9 @@
 constexpr int verysquare(int x) { return x * x; }
 
 const double mass = 9.8;
-constexpr double energy = mass * verysquare(56.6); // { dg-error "mass" }
+constexpr double energy = mass * verysquare(56.6); // { dg-error "mass" "" { xfail *-*-* } }
 
-int arr[(int)mass];		// { dg-error "mass" }
+int arr[(int)mass];		// { dg-error "mass" "" { xfail *-*-* } }
 
 float array[verysquare(9)];         // OK -- not C99 VLA
 
