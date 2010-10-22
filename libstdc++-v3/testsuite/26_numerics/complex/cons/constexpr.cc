941322b51ef1e8b706140467356dbaab4d2810a8
Benjamin Kosnik <bkoz@redhat.com>
2010-10-22 12:35:42 -0700

    2010-10-21  Benjamin Kosnik  <bkoz@redhat.com>
    	PR libstdc++/46134
    	* include/std/chrono: Use default constructor.
    	* testsuite/util/testsuite_common_types.h
    	(constexpr_default_constructible): Adjust condition.
    
    	PR libstdc++/46133
    	* include/std/complex: Adjust complex specialization default
    	constructors for constexpr.
    	* testsuite/26_numerics/complex/cons/constexpr.cc: Enable tests.

--- libstdc++-v3/testsuite/26_numerics/complex/cons/constexpr.cc
+++ libstdc++-v3/testsuite/26_numerics/complex/cons/constexpr.cc
@@ -24,18 +24,14 @@
 int main()
 {
   __gnu_test::constexpr_default_constructible test1;
-#if 0
   test1.operator()<std::complex<float>>();
   test1.operator()<std::complex<double>>();
   test1.operator()<std::complex<long double>>();
-#endif
 
   __gnu_test::constexpr_single_value_constructible test2;
-#if 0
   test2.operator()<std::complex<float>, float>();
   test2.operator()<std::complex<double>, double>();
   test2.operator()<std::complex<long double>, long double>();
-#endif
 
   return 0;
 }
