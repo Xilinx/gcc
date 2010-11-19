c6a42aaef1ef8d10144857809e7310c4b57335e5
bstarynk <bstarynk@138bc75d-0d04-0410-961f-82ee72b054a4>
2010-11-19 15:41:49 +0000

    2010-11-19  Basile Starynkevitch  <basile@starynkevitch.net> 	* gcc/testsuite/melt/topengpu-1.c: new file. 	* melt/xtramelt-opengpu.melt: Added pass.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/melt-branch@166946 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/melt/topengpu-1.c
@@ -0,0 +1,115 @@
+/* file gcc/testsuite/melt/topengpu-1.c; inspired from file
+   matrix/oper.c of the gsl library version 1.14
+   http://www.gnu.org/software/gsl/ */
+
+/* run in $GCCMELT_BUILD/gcc 
+ ./cc1 -fmelt=opengpu -fmelt-module-path=. -fmelt-source-path=. -fmelt-debug -O2 -c $GCCMELT_SOURCE/gcc/testsuite/melt/topengpu-1.c
+*/
+
+struct gsl_block_struct
+{
+  size_t size;
+  double *data;
+};
+
+typedef struct gsl_block_struct gsl_block;
+
+typedef struct
+{
+  size_t size1;
+  size_t size2;
+  size_t tda;
+  double *data;
+  gsl_block *block;
+  int owner;
+} gsl_matrix;
+
+typedef struct
+{
+  gsl_matrix matrix;
+} _gsl_matrix_view;
+
+/* Convert a string to a long integer.  */
+extern long int strtol (__const char *__restrict __nptr,
+			char **__restrict __endptr, int __base)
+  __attribute__ ((__nothrow__)) __attribute__ ((__nonnull__ (1)));
+
+/* Convert a string to a floating-point number.  */
+extern double strtod (__const char *__restrict __nptr,
+		      char **__restrict __endptr)
+  __attribute__ ((__nothrow__)) __attribute__ ((__nonnull__ (1)));
+
+
+extern __inline int __attribute__ ((__nothrow__)) atoi (__const char *__nptr)
+{
+  return (int) strtol (__nptr, (char **) ((void *) 0), 10);
+}
+
+extern __inline double
+  __attribute__ ((__nothrow__)) atof (__const char *__nptr)
+{
+  return strtod (__nptr, (char **) ((void *) 0));
+}
+
+extern void say_error (const char *msg);
+
+
+int
+gsl_matrix_add (gsl_matrix * a, const gsl_matrix * b)
+{
+  const size_t M = a->size1;
+  const size_t N = a->size2;
+
+  if (b->size1 != M || b->size2 != N)
+    {
+      say_error ("matrices must have same dimensions");
+      return -1;
+    }
+  else
+    {
+      const size_t tda_a = a->tda;
+      const size_t tda_b = b->tda;
+
+      size_t i, j;
+
+      for (i = 0; i < M; i++)
+	{
+	  for (j = 0; j < N; j++)
+	    {
+	      a->data[i * tda_a + j] += b->data[i * tda_b + j];
+	    }
+	}
+
+      return 0;
+    }
+}
+
+int
+gsl_matrix_sub (gsl_matrix * a, const gsl_matrix * b)
+{
+  const size_t M = a->size1;
+  const size_t N = a->size2;
+
+  if (b->size1 != M || b->size2 != N)
+    {
+      say_error ("matrices must have same dimensions");
+      return -1;
+    }
+  else
+    {
+      const size_t tda_a = a->tda;
+      const size_t tda_b = b->tda;
+
+      size_t i, j;
+
+      for (i = 0; i < M; i++)
+	{
+	  for (j = 0; j < N; j++)
+	    {
+	      a->data[i * tda_a + j] -= b->data[i * tda_b + j];
+	    }
+	}
+
+      return 0;
+    }
+}
