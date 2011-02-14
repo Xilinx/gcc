04c09b52742311a7e21e37d0a4b12f338a2f0e4a
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-02-14 19:00:52 +0000

    Permute the conversion and addition if one operand is a constant.
    gcc/
    
    2011-02-14  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR middle-end/47727
    	* explow.c (convert_memory_address_addr_space): Permute the
    	conversion and addition if one operand is a constant.
    
    gcc/testsuite/
    
    2011-02-14  H.J. Lu  <hongjiu.lu@intel.com>
    
    	PR middle-end/47727
    	* gcc.dg/pr47727.c: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/x32@170149 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/explow.c
+++ gcc/explow.c
@@ -383,18 +383,13 @@ convert_memory_address_addr_space (enum machine_mode to_mode ATTRIBUTE_UNUSED,
 
     case PLUS:
     case MULT:
-      /* For addition we can safely permute the conversion and addition
-	 operation if one operand is a constant and converting the constant
-	 does not change it or if one operand is a constant and we are
-	 using a ptr_extend instruction  (POINTERS_EXTEND_UNSIGNED < 0).
-	 We can always safely permute them if we are making the address
-	 narrower.  */
+      /* For addition we safely permute the conversion and addition
+	 operation if one operand is a constant since we can't generate
+	 new instructions.  We can always safely permute them if we are
+	 making the address narrower.  */
       if (GET_MODE_SIZE (to_mode) < GET_MODE_SIZE (from_mode)
 	  || (GET_CODE (x) == PLUS
-	      && CONST_INT_P (XEXP (x, 1))
-	      && (XEXP (x, 1) == convert_memory_address_addr_space
-				   (to_mode, XEXP (x, 1), as)
-                 || POINTERS_EXTEND_UNSIGNED < 0)))
+	      && CONST_INT_P (XEXP (x, 1))))
 	return gen_rtx_fmt_ee (GET_CODE (x), to_mode,
 			       convert_memory_address_addr_space
 				 (to_mode, XEXP (x, 0), as),
