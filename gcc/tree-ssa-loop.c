4b6ac7ee1afbe0d4a3733f717cd7e900db59e1f0
spop <spop@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-02-22 22:05:53 +0000

    Fix merge problem.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/graphite@170416 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/tree-ssa-loop.c
+++ gcc/tree-ssa-loop.c
@@ -265,17 +265,12 @@ gate_graphite_transforms (void)
      is turned on.  */
   if (flag_loop_block
       || flag_loop_interchange
-      || flag_tree_loop_linear
       || flag_loop_strip_mine
       || flag_graphite_identity
       || flag_loop_parallelize_all
       || flag_loop_flatten
       || flag_graphite_opencl)
     flag_graphite = 1;
-
-  /* Make flag_tree_loop_linear an alias of flag_loop_interchange.  */
-  if (flag_tree_loop_linear)
-    flag_loop_interchange = flag_tree_loop_linear;
 
   return flag_graphite != 0;
 }
