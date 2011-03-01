c1999fcc76eedba923738ef37d553b16af9b64ca
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-03-01 17:29:37 +0000

    Revert the last ix86_trampoline_init change.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/x32@170597 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -23408,10 +23408,7 @@ ix86_trampoline_init (rtx m_tramp, tree fndecl, rtx chain_value)
 	  mem = adjust_address (m_tramp, HImode, offset);
 	  emit_move_insn (mem, gen_int_mode (0xbb49, HImode));
 
-	  mem = adjust_address (m_tramp, ptr_mode, offset + 2);
-	  if (GET_MODE (fnaddr) != ptr_mode)
-	    fnaddr = simplify_gen_subreg (ptr_mode, fnaddr,
-					  GET_MODE (fnaddr), 0);
+	  mem = adjust_address (m_tramp, DImode, offset + 2);
 	  emit_move_insn (mem, fnaddr);
 	  offset += 10;
 	}
@@ -23420,7 +23417,7 @@ ix86_trampoline_init (rtx m_tramp, tree fndecl, rtx chain_value)
       mem = adjust_address (m_tramp, HImode, offset);
       emit_move_insn (mem, gen_int_mode (0xba49, HImode));
 
-      mem = adjust_address (m_tramp, ptr_mode, offset + 2);
+      mem = adjust_address (m_tramp, DImode, offset + 2);
       emit_move_insn (mem, chain_value);
       offset += 10;
 
