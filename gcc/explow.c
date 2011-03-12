166fef73e91a204b11fc32fdbc07f0a38c8cce76
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-03-12 15:53:34 +0000

    Also check VOIDmode.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/x32@170905 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/explow.c
+++ gcc/explow.c
@@ -611,7 +611,7 @@ copy_to_reg (rtx x)
 rtx
 copy_addr_to_reg (rtx x)
 {
-  if (GET_MODE (x) != Pmode)
+  if (GET_MODE (x) != VOIDmode && GET_MODE (x) != Pmode)
     x = convert_to_mode (Pmode, x, 1);
   return copy_to_mode_reg (Pmode, x);
 }
