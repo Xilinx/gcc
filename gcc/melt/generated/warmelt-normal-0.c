7c16fe1eec65764161f1d9ae8fc320baf1855bf4
bstarynk <bstarynk@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-04-05 06:07:08 +0000

    2011-04-05  Basile Starynkevitch  <basile@starynkevitch.net> 	{{Regenerated}} 	* melt/generated/warmelt-macro-0.c: Generate. 	* melt/generated/warmelt-genobj-0.c: Generate. 	* melt/generated/warmelt-normatch-0.c: Generate. 	* melt/generated/gt-melt-runtime-plugin.h: Generate. 	* melt/generated/warmelt-debug-0.c: Generate. 	* melt/generated/warmelt-normal-0.c: Generate. 	* melt/generated/warmelt-outobj-0.c: Generate. 	* melt/generated/warmelt-base-0.c: Generate. 	* melt/generated/meltrunsup.h: Generate. 	* melt/generated/meltrunsup-inc.c: Generate. 	* melt/generated/warmelt-first-0.c: Generate.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/melt-branch@171969 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/melt/generated/warmelt-normal-0.c
+++ gcc/melt/generated/warmelt-normal-0.c
@@ -3,11 +3,11 @@
 #ifndef MELTGCC_DYNAMIC_OBJSTRUCT
 /* version string of the gcc executable generating this file: */
 const char genversionstr_melt[]=
- "4.7.0 20110314 (experimental) [melt-branch revision 171820]";
+ "4.7.0 20110314 (experimental) [melt-branch revision 171966]";
 
 
 /* hash of preprocessed melt-run.h generating this file: */
-const char md5prepromeltrun_melt[]="bf053ef202a87644a104a7f62a99ca7f";
+const char md5prepromeltrun_melt[]="567a9f531326a9265a8423411f786f96";
 
 #endif /*MELTGCC_DYNAMIC_OBJSTRUCT*/
 
