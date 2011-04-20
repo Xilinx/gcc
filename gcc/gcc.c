2d2ffc9453712bf973d6a04cc8290e571a9aa2c4
eraman <eraman@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-04-20 19:11:01 +0000

    2011-04-19  Easwaran Raman  <eraman@google.com>
    	* gcc/gcc.c (asm_options): Pass --save-temps to assembler.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/google/main@172789 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/gcc.c
+++ gcc/gcc.c
@@ -775,12 +775,16 @@ static const char *cc1_options =
  %{fmudflap|fmudflapth:-fno-builtin -fno-merge-constants}\
  %{coverage:-fprofile-arcs -ftest-coverage}";
 
+/* If an assembler wrapper is used to invoke post-assembly tools
+   like MAO, --save-temps need to be passed to save the output of
+   such post-processing tools. This is not compatible with vanilla
+   binutils gas which does not recognize --save-temps.  */
 static const char *asm_options =
 "%{-target-help:%:print-asm-header()} "
 #if HAVE_GNU_AS
 /* If GNU AS is used, then convert -w (no warnings), -I, and -v
    to the assembler equivalents.  */
-"%{v} %{w:-W} %{I*} "
+"%{v} %{w:-W} %{I*} %{save-temps:--save-temps} "
 #endif
 "%a %Y %{c:%W{o*}%{!o*:-o %w%b%O}}%{!c:-o %d%w%u%O}";
 
