7e30c2455ba30aaa11fe274ebeeec2acc8e6a541
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-06-01 15:42:11 +0000

    * g++.dg/pph/c1return-5.cc: New. 	* g++.dg/pph/c1return-5.h: New.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/pph@174533 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/g++.dg/pph/c1return-5.cc
@@ -0,0 +1,1 @@
+#include "c1return-5.h"
---
+++ gcc/testsuite/g++.dg/pph/c1return-5.h
@@ -0,0 +1,16 @@
+#ifndef __PPH_GUARD_H
+#define __PPH_GUARD_H
+/* { dg-options "-mpreferred-stack-boundary=4" } */
+/* { dg-final { scan-assembler-not "and\[lq\]?\[^\\n\]*-64,\[^\\n\]*sp" } } */
+
+/* This compile only test is to detect an assertion failure in stack branch
+   development.  */
+struct bar
+{
+  int x;
+} __attribute__((aligned(64)));
+
+
+struct bar
+foo (void) { }
+#endif
