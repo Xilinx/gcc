23ab47781bfb9e2eb47b5be403751d5734c748e4
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-08-26 04:26:51 +0000

    * g++.dg/pph/x7rtti.cc: Adjust line numbers for expected errors.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/pph@178096 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/pph/x7rtti.cc
+++ gcc/testsuite/g++.dg/pph/x7rtti.cc
@@ -1,18 +1,18 @@
 // { dg-do run }
 // { dg-xfail-if "BOGUS" { "*-*-*" } { "-fpph-map=pph.map" } }
-// { dg-bogus "x7rtti.cc:21:0: warning: .__STDC_IEC_559_COMPLEX__. redefined .enabled by default." "" { xfail *-*-* } 0 }
-// { dg-bogus "x7rtti.cc:21:0: warning: .__STDC_ISO_10646__. redefined .enabled by default." "" { xfail *-*-* } 0 }
-// { dg-bogus "x7rtti.cc:21:0: warning: .__STDC_IEC_559__. redefined .enabled by default." "" { xfail *-*-* } 0 }
-// { dg-bogus "x7rtti.cc:24:32: error: no match for .operator" "" { xfail *-*-* } 0 }
-// { dg-bogus "x7rtti.cc:24:54: error: no match for .operator" "" { xfail *-*-* } 0 }
-// { dg-bogus "x7rtti.cc:25:32: error: no match for .operator" "" { xfail *-*-* } 0 }
-// { dg-bogus "x7rtti.cc:25:54: error: no match for .operator" "" { xfail *-*-* } 0 }
-// { dg-bogus "x7rtti.cc:26:32: error: no match for .operator" "" { xfail *-*-* } 0 }
-// { dg-bogus "x7rtti.cc:26:54: error: no match for .operator" "" { xfail *-*-* } 0 }
-// { dg-bogus "x7rtti.cc:27:32: error: no match for .operator" "" { xfail *-*-* } 0 }
-// { dg-bogus "x7rtti.cc:27:54: error: no match for .operator" "" { xfail *-*-* } 0 }
-// { dg-bogus "x7rtti.cc:28:1: error: redefinition of .const char _ZTS15non_polymorphic ..." "" { xfail *-*-* } 0 }
-// { dg-bogus "x7rtti.cc:28:1: error: redefinition of .const char _ZTS11polymorphic ..." "" { xfail *-*-* } 0 }
+// { dg-bogus "x7rtti.cc:19:0: warning: .__STDC_IEC_559_COMPLEX__. redefined .enabled by default." "" { xfail *-*-* } 0 }
+// { dg-bogus "x7rtti.cc:19:0: warning: .__STDC_ISO_10646__. redefined .enabled by default." "" { xfail *-*-* } 0 }
+// { dg-bogus "x7rtti.cc:19:0: warning: .__STDC_IEC_559__. redefined .enabled by default." "" { xfail *-*-* } 0 }
+// { dg-bogus "x7rtti.cc:22:26: error: no match for .operator" "" { xfail *-*-* } 0 }
+// { dg-bogus "x7rtti.cc:23:22: error: no match for .operator" "" { xfail *-*-* } 0 }
+// { dg-bogus "x7rtti.cc:24:29: error: no match for .operator" "" { xfail *-*-* } 0 }
+// { dg-bogus "x7rtti.cc:25:22: error: no match for .operator" "" { xfail *-*-* } 0 }
+// { dg-bogus "x7rtti.cc:26:29: error: no match for .operator" "" { xfail *-*-* } 0 }
+// { dg-bogus "x7rtti.cc:27:22: error: no match for .operator" "" { xfail *-*-* } 0 }
+// { dg-bogus "x7rtti.cc:28:29: error: no match for .operator" "" { xfail *-*-* } 0 }
+// { dg-bogus "x7rtti.cc:29:22: error: no match for .operator" "" { xfail *-*-* } 0 }
+// { dg-bogus "x7rtti.cc:33:1: error: redefinition of .const char _ZTS15non_polymorphic ..." "" { xfail *-*-* } 0 }
+// { dg-bogus "x7rtti.cc:33:1: error: redefinition of .const char _ZTS11polymorphic ..." "" { xfail *-*-* } 0 }
 
 #include "x5rtti1.h"
 #include "x5rtti2.h"
