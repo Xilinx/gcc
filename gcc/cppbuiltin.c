0f37f9c63692f0aae3a313e3ee6be4f7c848a5c2
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-09-07 22:00:58 +0000

    Define __cilk CPP macro.
    gcc/
    
    2011-09-06  Balaji V. Iyer  <balaji.v.iyer@intel.com>
    
    	* cppbuiltin.c (define_builtin_macros_for_compliation_flags): Added
    	the __cilk keyword and set it to 200.
    
    gcc/testsuite/
    
    2011-09-07  Balaji V. Iyer  <balaji.v.iyer@intel.com>
    
    	* gcc.dg/cilk-plus/test__cilk.c: New.
    	* g++.dg/cilk-plus/test__cilk.cpp: New.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/cilkplus@178664 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/cppbuiltin.c
+++ gcc/cppbuiltin.c
@@ -97,6 +97,9 @@ define_builtin_macros_for_compilation_flags (cpp_reader *pfile)
 
   cpp_define_formatted (pfile, "__FINITE_MATH_ONLY__=%d",
 			flag_finite_math_only);
+
+  if (flag_enable_cilk)
+    cpp_define_formatted(pfile, "__cilk=%d", 200);
 }
 
 
---
+++ gcc/testsuite/gcc.dg/cilk-plus/test__cilk.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void)
+{
+  printf ("__cilk = %d\n", __cilk);
+  return 0;
+}
