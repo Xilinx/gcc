99de37bb0d762dba78d5bb34dc5cb688c0290b5e
hjl <hjl@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-09-09 16:12:50 +0000

    Add the standard GCC header to pragma_simd.c.
    2011-09-09  Balaji V. Iyer  <balaji.v.iyer@intel.com>
    
    	* pragma_simd.c: Added the standard GCC header.
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/cilkplus@178735 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/pragma_simd.c
+++ gcc/pragma_simd.c
@@ -1,8 +1,25 @@
-/* Routines to handle Pragma SIMD capabilities by the vectorizer
- * Copyright (C) 2011
- * Contributed by Balaji V. Iyer <balaji.v.iyer@intel.com>
- *
- */
+/* This file is part of the Intel(R) Cilk(TM) Plus support
+   This file contains routines to handle PRAGMA SIMD assignments by the
+   vectorizer.
+   Copyright (C) 2011  Free Software Foundation, Inc.
+   Contributed by Balaji V. Iyer <balaji.v.iyer@intel.com>,
+		  Intel Corporation
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
 
 #include "config.h"
 #include "system.h"
