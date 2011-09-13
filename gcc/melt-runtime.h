1e1f549d5498c16cf4674b4d51f14d1c5b8d49b0
bstarynk <bstarynk@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-09-13 18:31:35 +0000

    2011-09-13  Basile Starynkevitch  <basile@starynkevitch.net> 	* melt-runtime.h (MELT_VERSION_STRING): Bumped to 0.9rc2
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/melt-branch@178828 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/melt-runtime.h
+++ gcc/melt-runtime.h
@@ -98,7 +98,7 @@ extern char* melt_gccversionstr;
 extern const int melt_gcc_version;
 
 /* the version string of MELT */
-#define MELT_VERSION_STRING "0.9rc1"
+#define MELT_VERSION_STRING "0.9rc2"
 
 /* return a read only version string */
 extern const char* melt_version_str(void);
