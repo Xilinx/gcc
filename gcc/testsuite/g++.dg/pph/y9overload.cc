3cfeaa9380e8e8e2042a6db0c62d7cc27e7545ae
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-10-05 20:15:51 +0000

    * g++.dg/pph/y9overlaad.cc: Add comment on expected 	diagnostic.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/pph@179576 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/pph/y9overload.cc
+++ gcc/testsuite/g++.dg/pph/y9overload.cc
@@ -1,6 +1,11 @@
 #include "x0overload1.h"
 #include "x0overload2.h"
 #include "x1overload3.h"
+// When this test case is compiled in PPH mode, we should be generating
+// a diagnostic because overload resolution is different than in
+// the non PPH compile.  The call to o() from x1overload3.h should
+// resolve differently when x0overload1.h and x0overload2.h are PPH
+// images.
 // { dg-error "inconsistent overload resolution" "" { xfail *-*-* } }
 
 int g()
