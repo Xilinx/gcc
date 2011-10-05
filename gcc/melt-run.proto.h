881a399f71a2068d0654b1deed4a5f36a703b2a1
bstarynk <bstarynk@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-10-05 17:29:07 +0000

    2011-10-05  Basile Starynkevitch  <basile@starynkevitch.net> 	* melt-run.proto.h: Use c-family/ for includes of c-pragma.h 	etc... also when GCC_VERSION is for 4.7.
    	* testsuite/melt/tvariadic.melt: Renamed as
    	* testsuite/melt/tvariadic-1.melt: New.
    	* testsuite/melt/tvariadic-2.melt: New.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/melt-branch@179562 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/melt-run.proto.h
+++ gcc/melt-run.proto.h
@@ -59,7 +59,7 @@ along with GCC; see the file COPYING3.   If not see
 /* Headers from c-family/ should be included directly with GCC4.6, but
    not with GCC 4.7 or when compiling with a C++ compiler. And they
    need to be include before diagnostics.h.  */
-#if defined(GCCPLUGIN_VERSION) || MELT_GCC_VERSION>4006 || defined(__cplusplus)
+#if defined(GCCPLUGIN_VERSION) || MELT_GCC_VERSION>4006 || GCC_VERSION>4006 || defined(__cplusplus)
 #include "c-family/c-pragma.h"
 #include "c-family/c-pretty-print.h"
 #else
