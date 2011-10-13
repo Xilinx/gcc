9cc5f99678d8091e49df9a389669c757957b712b
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-10-13 15:33:28 +0000

    * g++.dg/pph/c1limits-externalid.cc: Add shorter timeout. 	Document failure mode.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/pph@179924 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/pph/c1limits-externalid.cc
+++ gcc/testsuite/g++.dg/pph/c1limits-externalid.cc
@@ -1,1 +1,6 @@
+/* FIXME pph - The following timeout may cause failures on slow targets.
+   In general it takes no longer than a couple of seconds to compile
+   this test, but the new merging code is having trouble with this.  */
+/* { dg-timeout 15 } */
+/* { dg-xfail-if "MERGE INFINITE LOOP" { *-*-* } { "-fpph-map=pph.map" } } */
 #include "c0limits-externalid.h"
