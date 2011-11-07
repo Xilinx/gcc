68f23febd3dfc68e62e0b29cd2330342d3dcec10
singhai <singhai@138bc75d-0d04-0410-961f-82ee72b054a4>
2011-11-07 22:16:19 +0000

    2011-11-07   Sharad Singhai  <singhai@google.com>
           * gcc.c (cc1_options): Added -fno-early-inlining for coverage.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/google/main@181126 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/gcc.c
+++ gcc/gcc.c
@@ -791,7 +791,7 @@ static const char *cc1_options =
  %{!fsyntax-only:%{S:%W{o*}%{!o*:-o %b.s}}}\
  %{fsyntax-only:-o %j} %{-param*}\
  %{fmudflap|fmudflapth:-fno-builtin -fno-merge-constants}\
- %{coverage:-fprofile-arcs -ftest-coverage}";
+ %{coverage:-fprofile-arcs -ftest-coverage -fno-early-inlining}";
 
 /* If an assembler wrapper is used to invoke post-assembly tools
    like MAO, --save-temps need to be passed to save the output of
