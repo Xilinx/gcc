1b295546f43e994e2b5fa560a28a682a99d517a6
H.J. Lu <hjl.tools@gmail.com>
2011-11-10 14:29:11 -0800

    Correct merge failure.

--- gcc/testsuite/gcc.target/i386/incoming-8.c
+++ gcc/testsuite/gcc.target/i386/incoming-8.c
@@ -1,11 +1,6 @@
 /* PR target/40838 */
-<<<<<<< HEAD
-/* { dg-do compile { target { { ! *-*-darwin* } && ilp32 } } } */
-/* { dg-options "-w -mstackrealign -O3 -msse2 -mpreferred-stack-boundary=4" } */
-=======
 /* { dg-do compile { target { { ! *-*-darwin* } && ia32 } } } */
-/* { dg-options "-w -mstackrealign -O3 -msse2 -mno-avx -mpreferred-stack-boundary=4" } */
->>>>>>> 06a49c5... Add x32 support to GCC testsuite.
+/* { dg-options "-w -mstackrealign -O3 -msse2 -mpreferred-stack-boundary=4" } */
 
 float
 foo (float f)
