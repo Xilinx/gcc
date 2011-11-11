35050696ab1370cf313768d0328ec826489a0d59
H.J. Lu <hjl.tools@gmail.com>
2011-11-11 10:49:15 -0800

    Update comments for _Unwind_SetGRValue.

--- gcc/unwind-dw2.c
+++ gcc/unwind-dw2.c
@@ -289,7 +289,7 @@ _Unwind_SetGRValue (struct _Unwind_Context *context, int index,
 {
   index = DWARF_REG_TO_UNWIND_COLUMN (index);
   gcc_assert (index < (int) sizeof(dwarf_reg_size_table));
-  /* Stack pointer may be smaller than _Unwind_Context_Reg_Va.  */ 
+  /* Return column size may be smaller than _Unwind_Context_Reg_Va.  */ 
   gcc_assert (dwarf_reg_size_table[index] <= sizeof (_Unwind_Context_Reg_Val));
 
   context->by_value[index] = 1;
