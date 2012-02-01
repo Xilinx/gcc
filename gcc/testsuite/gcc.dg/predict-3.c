166f404f97cfd0e6fd53c86354439bdc1bfc36d5
dehao <dehao@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-02-01 17:08:52 +0000

    Implement a new static profiling heuristic: comparing induction variable to loop bound variable has certain prediction patterns.
    	* predict.c (strips_small_constant): New
    	(get_base_value): New
    	(find_ssa_name_in_assign_stmt): New
    	(is_comparison_with_loop_invariant_p): New
    	(expr_coherent_p): New
    	(predict_iv_comparison): New
    	(predict_loops): Add heuristic for loop-nested branches that compare an
    	induction variable to a loop bound variable.
    	* predict.def (PRED_LOOP_IV_COMPARE): New macro
    	* testsuite/gcc.dg/predict-1.c: New test
    	* testsuite/gcc.dg/predict-2.c: New test
    	* testsuite/gcc.dg/predict-3.c: New test
    	* testsuite/gcc.dg/predict-4.c: New test
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/google/main@183805 138bc75d-0d04-0410-961f-82ee72b054a4

---
+++ gcc/testsuite/gcc.dg/predict-3.c
@@ -0,0 +1,19 @@
+/* { dg-do compile } */
+/* { dg-options "-O2 -fdump-tree-profile_estimate" } */
+
+extern int global;
+
+int bar(int);
+
+void foo (int bound)
+{
+  int i, ret = 0;
+  for (i = 0; i <= bound; i++)
+    {
+      if (i != bound)
+	global += bar (i);
+    }
+}
+
+/* { dg-final { scan-tree-dump "loop iv compare heuristics" "profile_estimate"} } */
+/* { dg-final { cleanup-tree-dump "profile_estimate" } } */
