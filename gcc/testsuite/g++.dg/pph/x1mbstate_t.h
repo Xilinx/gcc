4f09de0d75fed9fb6dc69f6ffb67f6f39d70dd0b
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-03-21 17:39:06 +0000

    Add expected error in x1mbstate_t.h.
    The fix in http://gcc.gnu.org/ml/gcc-patches/2012-03/msg01441.html did not
    address the bogus error message that we are now getting.
    
    2012-03-21   Diego Novillo  <dnovillo@google.com>
    
            * gcc/testsuite/g++.dg/pph/x1mbstate_t.h: Add expected error.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/pph@185608 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/pph/x1mbstate_t.h
+++ gcc/testsuite/g++.dg/pph/x1mbstate_t.h
@@ -4,5 +4,5 @@
 // Name lookup for std::mbstate_t was failingfails here.  Instead of returning
 // the global type_decl for mbstate_t, it was returning the
 // "usings ::mbstate_t" declaration.
-typedef std::mbstate_t state_type;
+typedef std::mbstate_t state_type; // { dg-error "'mbstate_t' in namespace 'std' does not name a type" }
 #endif
