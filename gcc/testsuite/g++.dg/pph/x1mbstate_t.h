4064a085a66b982ee5ae57a83a879df7dadcabfc
dnovillo <dnovillo@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-03-23 13:07:50 +0000

    2012-03-23   Diego Novillo  <dnovillo@google.com>
    	* g++.dg/pph/x1mbstate_t.h: Adjust failure pattern.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/pph@185736 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/g++.dg/pph/x1mbstate_t.h
+++ gcc/testsuite/g++.dg/pph/x1mbstate_t.h
@@ -1,8 +1,10 @@
+// { xfail-if "" { "*-*-*" } { "-fpph-map=pph.map" } }
+
 #ifndef _X1_MBSTATE_H
 #define _X1_MBSTATE_H
 #include "x0mbstate_t.h"
 // Name lookup for std::mbstate_t was failingfails here.  Instead of returning
 // the global type_decl for mbstate_t, it was returning the
 // "usings ::mbstate_t" declaration.
-typedef std::mbstate_t state_type; // { dg-error "'mbstate_t' in namespace 'std' does not name a type" }
+typedef std::mbstate_t state_type;  // { dg-bogus "'mbstate_t' in namespace 'std' does not name a type" "" { xfail *-*-* } }
 #endif
