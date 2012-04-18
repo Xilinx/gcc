82e0059092c45a9187fb553c5096be0d07643b62
Edgar E. Iglesias <edgar.iglesias@gmail.com>
2012-04-19 00:07:40 +0200

    [LOCAL]: Testsuite - explicitly add -fivopts for tests that depend on it
    (test gcc/testsuite/gcc.dg/tree-ssa/ivopts-lt.c doesnt exist in 4.6 branch)
    
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@gmail.com>

--- gcc/testsuite/gcc.dg/tree-ssa/loop-2.c
+++ gcc/testsuite/gcc.dg/tree-ssa/loop-2.c
@@ -1,7 +1,7 @@
 /* A test for strength reduction and induction variable elimination.  */
 
 /* { dg-do compile } */
-/* { dg-options "-O1 -fdump-tree-optimized" } */
+/* { dg-options "-O1 -fivopts -fdump-tree-optimized" } */
 /* { dg-require-effective-target size32plus } */
 
 /* Size of this structure should be sufficiently weird so that no memory
