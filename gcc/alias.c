0680da650a36b1b2245bc8d92876dbd243bd311c
bstarynk <bstarynk@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-10-18 10:23:22 +0000

    2012-10-18  Basile Starynkevitch  <basile@starynkevitch.net>
    	MELT branch merged with trunk rev 192552 using svnmerge.py
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/melt-branch@192557 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/alias.c
+++ gcc/alias.c
@@ -1640,6 +1640,9 @@ find_base_term (rtx x)
       if (!val)
 	return ret;
 
+      if (cselib_sp_based_value_p (val))
+	return static_reg_base_value[STACK_POINTER_REGNUM];
+
       f = val->locs;
       /* Temporarily reset val->locs to avoid infinite recursion.  */
       val->locs = NULL;
@@ -2757,6 +2760,39 @@ memory_modified_in_insn_p (const_rtx mem, const_rtx insn)
   memory_modified = false;
   note_stores (PATTERN (insn), memory_modified_1, CONST_CAST_RTX(mem));
   return memory_modified;
+}
+
+/* Return TRUE if the destination of a set is rtx identical to
+   ITEM.  */
+static inline bool
+set_dest_equal_p (const_rtx set, const_rtx item)
+{
+  rtx dest = SET_DEST (set);
+  return rtx_equal_p (dest, item);
+}
+
+/* Like memory_modified_in_insn_p, but return TRUE if INSN will
+   *DEFINITELY* modify the memory contents of MEM.  */
+bool
+memory_must_be_modified_in_insn_p (const_rtx mem, const_rtx insn)
+{
+  if (!INSN_P (insn))
+    return false;
+  insn = PATTERN (insn);
+  if (GET_CODE (insn) == SET)
+    return set_dest_equal_p (insn, mem);
+  else if (GET_CODE (insn) == PARALLEL)
+    {
+      int i;
+      for (i = 0; i < XVECLEN (insn, 0); i++)
+	{
+	  rtx sub = XVECEXP (insn, 0, i);
+	  if (GET_CODE (sub) == SET
+	      &&  set_dest_equal_p (sub, mem))
+	    return true;
+	}
+    }
+  return false;
 }
 
 /* Initialize the aliasing machinery.  Initialize the REG_KNOWN_VALUE
