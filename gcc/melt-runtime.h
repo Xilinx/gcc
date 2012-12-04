f766cb4b0bc55b80a0c35d8769c86ad994ed40c2
bstarynk <bstarynk@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-12-04 13:57:56 +0000

    2012-12-04  Basile Starynkevitch  <basile@starynkevitch.net>
    	* melt-runtime.h (MELT_VERSION_STRING): Bump to 0.9.8-rc0
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/melt-branch@194140 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/melt-runtime.h
+++ gcc/melt-runtime.h
@@ -113,7 +113,7 @@ MELT_EXTERN int melt_count_runtime_extensions;
 /* The version string of MELT; this is parsed by make, so spaces are
    important!  That version string is extracted by scripts or
    makefiles... */
-#define MELT_VERSION_STRING "0.9.7-post"
+#define MELT_VERSION_STRING "0.9.8-rc0"
 
 /* return a read only version string */
 extern const char* melt_version_str(void);
