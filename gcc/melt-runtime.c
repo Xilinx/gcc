3f0bee3d9b0aaf32e961bf1e86920d65c6064aa0
bstarynk <bstarynk@138bc75d-0d04-0410-961f-82ee72b054a4>
2012-12-04 11:45:01 +0000

    2012-12-04  Basile Starynkevitch  <basile@starynkevitch.net>
    	* melt-runtime.c (MELT_DEFAULT_DEBUG_DEPTH): Increase to 9.
    	(MELT_MAXIMAL_DEBUG_DEPTH): Increase to 28.
    
    	* melt/warmelt-normal.melt (normexp_box): More, but still not
    	working.
    
    	* melt/warmelt-modes.melt (eval_docmd, evalfile_docmd)
    	(repl_processor): The :dbgi_maxdepth uses debug_depth.
    
    
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/melt-branch@194127 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/melt-runtime.c
+++ gcc/melt-runtime.c
@@ -892,9 +892,9 @@ melt_argument (const char* argname)
 /* the debug depth for MELT debug_msg .... */
 int melt_debug_depth (void)
 {
-#define MELT_DEFAULT_DEBUG_DEPTH 7
+#define MELT_DEFAULT_DEBUG_DEPTH 9
 #define MELT_MINIMAL_DEBUG_DEPTH 2
-#define MELT_MAXIMAL_DEBUG_DEPTH 25
+#define MELT_MAXIMAL_DEBUG_DEPTH 28
   static int d;
   if (MELT_UNLIKELY(!d)) {
     const char* dbgdepthstr = melt_argument ("debug-depth");
