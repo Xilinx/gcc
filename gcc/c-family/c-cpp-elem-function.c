22022cd00f7a97f9b6e10f78e8c54ae8a5fa4275
bviyer <bviyer@138bc75d-0d04-0410-961f-82ee72b054a4>
2013-01-22 23:22:43 +0000

    Removed setting ASSEMBLER NAME in elemental function.
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/cilkplus@195390 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/c-family/c-cpp-elem-function.c
+++ gcc/c-family/c-cpp-elem-function.c
@@ -770,7 +770,6 @@ elem_fn_create_fn (tree fndecl)
       fix_elem_fn_return_value (new_masked_fn, elem_fn_values, induction_var);
       segment_params_for_reg_size (new_masked_fn);
       call_graph_add_fn (new_masked_fn);
-      SET_DECL_ASSEMBLER_NAME (new_masked_fn, DECL_NAME (new_masked_fn));
       DECL_ELEM_FN_ALREADY_CLONED (new_masked_fn) = true;
       if (DECL_STRUCT_FUNCTION (new_masked_fn))
 	DECL_STRUCT_FUNCTION (new_masked_fn)->elem_fn_already_cloned = true;
@@ -803,7 +802,6 @@ elem_fn_create_fn (tree fndecl)
 				induction_var);
       segment_params_for_reg_size (new_unmasked_fn);
       call_graph_add_fn (new_unmasked_fn);
-      SET_DECL_ASSEMBLER_NAME (new_unmasked_fn, DECL_NAME (new_unmasked_fn));
       DECL_ELEM_FN_ALREADY_CLONED (new_unmasked_fn) = true;
       if (DECL_STRUCT_FUNCTION (new_unmasked_fn))
 	DECL_STRUCT_FUNCTION (new_unmasked_fn)->elem_fn_already_cloned = true;
