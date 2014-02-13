24b35cd06ab1f74b1f084e0a7919fa13a1fc018d
David Holsgrove <david.holsgrove@xilinx.com>
2014-02-14 09:36:45 +1000

    Allow MicroBlaze .weakext pattern in regex match
    Extend regex pattern to include optional ext at the end of
    .weak to match the MicroBlaze weak label .weakext
    
    ChangeLog/testsuite
    
    2014-02-14  David Holsgrove <david.holsgrove@xilinx.com>
    
     * gcc/testsuite/g++.dg/abi/rtti3.C: Extend scan-assembler
       pattern to take optional ext after .weak.
     * gcc/testsuite/g++.dg/abi/thunk4.C: Likewise.
    
    Signed-off-by: David Holsgrove <david.holsgrove@xilinx.com>

--- gcc/testsuite/g++.dg/abi/rtti3.C
+++ gcc/testsuite/g++.dg/abi/rtti3.C
@@ -3,8 +3,8 @@
 
 // { dg-require-weak "" }
 // { dg-skip-if "Linkonce not weak" { *-*-mingw* *-*-cygwin } { "*" } { "" } }
-// { dg-final { scan-assembler ".weak\[ \t\]_?_ZTSPP1A" { target { ! { *-*-darwin* } } } } }
-// { dg-final { scan-assembler-not ".weak\[ \t\]_?_ZTIPP1A" { target { ! { *-*-darwin* } } } } }
+// { dg-final { scan-assembler ".weak(ext)?\[ \t\]_?_ZTSPP1A" { target { ! { *-*-darwin* } } } } }
+// { dg-final { scan-assembler-not ".weak(ext)?\[ \t\]_?_ZTIPP1A" { target { ! { *-*-darwin* } } } } }
 // { dg-final { scan-assembler ".weak_definition\[ \t\]_?_ZTSPP1A" { target { *-*-darwin* } } } }
 // { dg-final { scan-assembler-not ".weak_definition\[ \t\]_?_ZTIPP1A" { target { *-*-darwin* } } } }
 
